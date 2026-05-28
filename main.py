925229d0e50ab5f49fd6e9047f3f705619f98cc6
neerajkorrapati <neerajkorrapati36@gmail.com>
2026-05-28 13:30:06 +0530

    phase_3_added_boss_and_player_final_winning_lines_through api integration

--- main.py
+++ main.py
@@ -12,14 +12,32 @@ def main():
     strike_attack=13
     magic_attack=15
     defense_move=5 #if defense employed, player hp will increse by 5
+    
     response=client.models.generate_content(
     model="gemini-2.5-flash",
     contents="generate a single scary introductory line for the boss ,keep it less than 25 words",
     config={
         "temperature":0.8
         }
+    )
 
-)
+    response_boss=client.models.generate_content(
+                model="gemini-2.5-flash",
+                contents="generate  a  fearsome line for the boss after defeating the player who is the hero.add evil laugh and something sadistic in the next line",
+                config={
+                    "temperature":0.8
+                }
+            )
+    response_player=client.models.generate_content(
+                model="gemini-2.5-flash",
+                contents="generate a  single heroic line for the player who is a hero, after defeating the boss who was a threat to the kingdom,(the player is a prince).",
+                config={
+                    "temperature":0.8
+                }
+            )
+
+
+
     print("Boss Introduction: ",response.text)
 
 
@@ -63,8 +81,12 @@ def main():
         player_hp=max(player_hp,0)
         if boss_hp==0:
             print("battle ended... player wins!!")
+            print("player's final words: ...",response_player.text)
         elif player_hp==0:
+        
             print("battle ended... boss wins!!")
+            print("Boss's final words:  ...",response_boss.text)
+
     #print("battle ended... player wins!!")
 if __name__=="__main__":
     main()
